02cb72e06a22629da4c446ef8b2306bc7d8f8774
Ricardo <146256152+ricode-icaito@users.noreply.github.com>
2023-11-08 16:31:46 -0600

    Add files via upload

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Esto funciona');
+  const h1 = document.querySelector('h1');
+  console.log(h1);
+  //h1.textContent = 'Segundo titulo';
+})
---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*console.log("Eventos en javascript");
+alert("Eventos.js");
+var privada = "calandria";
+console.log(privada);
+let casa144;
+casa144 = "Fam. Diosdado Sierra";
+console.log(typeof casa144);
+const CAS144 = "Ricardo, Gabriela, Brenda, Benjamin y ricky";
+console.log(CAS144);
+var num = 5;
+console.log(typeof num);
+let texto = "familia";
+console.log(typeof texto);
+var escribe = window.prompt("Escribe tu oficio:");
+console.log(escribe);
+alert(escribe);*/
